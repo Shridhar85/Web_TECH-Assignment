eb95bbacaf2c92c9db072abaed7fb255a137f6b5
Shridhar85 <78369282+Shridhar85@users.noreply.github.com>
2021-11-01 10:32:33 +0530

    Add files via upload

---
+++ 2006062_SHRIDHAR_KUMAR_JHA_JS_linking/script.js
@@ -0,0 +1,2 @@
+document.body.innerHTML='<h2>Linking javscript file to  html page using<span> external linking</span>.we can link html files to javascript file using external and internal linking. In external linking we make an external javaScript file and link it with the html file. In internal linking we write our javascript code inside that html file under script tag.<h2>';
+
